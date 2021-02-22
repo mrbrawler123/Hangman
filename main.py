7aa75a7a2bb1fde3fbff436f6fefc977fc526d3c
Omkaar <omkaar.nerurkar@gmail.com>
2021-02-22 15:22:01 +0000

    formatted

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ wl = response.text
 wlist = wl.split("\n")
 words_list = []
 for i in wlist:
-  if len(i) > 3:
-    words_list.append(i)
+	if len(i) > 3:
+		words_list.append(i)
 import time
+
+
 def find_letters(letter, word):
-  index = []
-  for i in range(len(word)):
-    if letter == word[i]:
-      index.append(i)
-    if letter != word[i]:
-      continue
-  return index
+	index = []
+	for i in range(len(word)):
+		if letter == word[i]:
+			index.append(i)
+		if letter != word[i]:
+			continue
+	return index
+
+
 time.sleep(1)
 print("HANGMAN")
 time.sleep(3)
@@ -49,7 +53,7 @@ while tries != len(word) + 5:
 		print(f"Congratulations! You guessed the word in {tries} chances!")
 		break
 	if len(s) == 1 and word.count(s) > 0:
-		for a in find_letters(s,word):
+		for a in find_letters(s, word):
 			answer[a] = s
 			correct = correct + 1
 		print(f"Yay! You guessed correctly!")
@@ -58,14 +62,18 @@ while tries != len(word) + 5:
 			break
 		if tries == len(word) + 5:
 			print(f"You have lost! The word was {word}!")
-		print(f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}")
+		print(
+		    f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}"
+		)
 	if word.count(s) == 0:
-		tries = tries + 1  
+		tries = tries + 1
 		print(f"You guessed wrong!")
 		if correct == len(word):
 			print(f"Congratulations! You guessed the word in {tries} chances!")
 			break
 		if tries == len(word) + 5:
 			print(f"You have lost! The word was {word}!")
-		print(f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}")  
+		print(
+		    f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}"
+		)
 	final_answer = separator.join(answer)
